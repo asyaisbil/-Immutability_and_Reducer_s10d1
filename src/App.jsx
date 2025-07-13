c346766065e25248c75e5799f382d6d4d019e0f7
asyaisbil <asyaisbill@gmail.com>
2025-07-13 21:39:34 +0300

    project_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import TotalDisplay from './components/TotalDisplay.jsx';
 import CalcButton from './components/CalcButton.jsx';
+import reducer, { initialState } from './store/reducers.jsx';
+import {
+  applyNumber,
+  changeOperation,
+  clearDisplay,
+  result,
+  typeToScreen,
+  memoryAdd,
+  memoryRecall,
+  memoryClear,
+} from './store/actions.jsx';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const eventNumberHandler = (event) => {
+    dispatch(typeToScreen(event.target.value));
+  };
+
+  const eventOperationHandler = (event) => {
+    dispatch(changeOperation(event.target.value));
+  };
+
+  const eventClearHandler = () => {
+    dispatch(clearDisplay());
+  };
+
+  const eventResultHandler = () => {
+    dispatch(result());
+  };
+
+  const eventMemoryAddHandler = () => {
+    dispatch(memoryAdd());
+  };
+
+  const eventMemoryRecallHandler = () => {
+    dispatch(memoryRecall());
+  };
+
+  const eventMemoryClearHandler = () => {
+    dispatch(memoryClear());
+  };
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -13,50 +54,50 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            <TotalDisplay value={'X'} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
               <span id="operation">
-                <b>Operation:</b> X
+                <b>Operation:</b> {state.operation}
               </span>
               <span id="memory">
-                <b>Memory:</b> X
+                <b>Memory:</b> {state.memory}
               </span>
             </div>
             <div className="row">
-              <CalcButton value={'M+'} />
-              <CalcButton value={'MR'} />
-              <CalcButton value={'MC'} />
+              <CalcButton value={'M+'} onClick={eventMemoryAddHandler} />
+              <CalcButton value={'MR'} onClick={eventMemoryRecallHandler} />
+              <CalcButton value={'MC'} onClick={eventMemoryClearHandler} />
             </div>
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
-            </div>
-
-            <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={1} onClick={eventNumberHandler} />
+              <CalcButton value={2} onClick={eventNumberHandler} />
+              <CalcButton value={3} onClick={eventNumberHandler} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={4} onClick={eventNumberHandler} />
+              <CalcButton value={5} onClick={eventNumberHandler} />
+              <CalcButton value={6} onClick={eventNumberHandler} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={7} onClick={eventNumberHandler} />
+              <CalcButton value={8} onClick={eventNumberHandler} />
+              <CalcButton value={9} onClick={eventNumberHandler} />
             </div>
             <div className="row">
-              <CalcButton value={'+'} />
-              <CalcButton value={0} />
-              <CalcButton value={'-'} />
+              <CalcButton value={'+'} onClick={eventOperationHandler} />
+              <CalcButton value={0} onClick={eventNumberHandler} />
+              <CalcButton value={'-'} onClick={eventOperationHandler} />
             </div>
             <div className="row">
-              <CalcButton value={'*'} />
-              <CalcButton value={'/'} />
-              <CalcButton value={'CE'} />
+              <CalcButton value={'*'} onClick={eventOperationHandler} />
+              <CalcButton value={'/'} onClick={eventOperationHandler} />
+              <CalcButton value={'CE'} onClick={eventClearHandler} />
             </div>
 
             <div className="row eq_button">
-              <CalcButton value={'='} />
+              <CalcButton value={'='} onClick={eventResultHandler} />
             </div>
           </form>
         </div>
--- src/store/reducers.jsx
+++ src/store/reducers.jsx
@@ -1,9 +1,19 @@
-import { APPLY_NUMBER, CHANGE_OPERATION } from '../actions/actions.jsx';
+import {
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  RESULT,
+  TYPE_TO_SCREEN,
+  MEMORY_ADD,
+  MEMORY_RECALL,
+  MEMORY_CLEAR,
+} from './actions.jsx';
 
 export const initialState = {
-  total: 100,
-  operation: '*',
-  memory: 100,
+  total: 0,
+  operation: '+',
+  memory: 0,
+  temp: 0,
 };
 
 const calculateResult = (num1, num2, operation) => {
@@ -29,10 +39,51 @@ const reducer = (state, action) => {
         total: calculateResult(state.total, action.payload, state.operation),
       };
 
+    case TYPE_TO_SCREEN:
+      return {
+        ...state,
+        total:
+          state.total === 0
+            ? action.payload
+            : state.total.toString() + action.payload.toString(),
+      };
+
+    case RESULT:
+      return {
+        ...state,
+        total: calculateResult(state.temp, state.total, state.operation),
+      };
+
     case CHANGE_OPERATION:
       return {
         ...state,
         operation: action.payload,
+        total: 0,
+        temp: state.total,
+      };
+
+    case CLEAR_DISPLAY:
+      return {
+        ...state,
+        total: 0,
+      };
+
+    case MEMORY_ADD:
+      return {
+        ...state,
+        memory: state.total,
+      };
+
+    case MEMORY_RECALL:
+      return {
+        ...state,
+        total: state.memory,
+      };
+
+    case MEMORY_CLEAR:
+      return {
+        ...state,
+        memory: 0,
       };
 
     default:
